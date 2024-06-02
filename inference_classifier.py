f5e8f44e658cbe93e7d943bcd70d995884fe2b53
Varshini2115 <158021943+Varshini2115@users.noreply.github.com>
2024-06-02 11:51:00 +0530

    Update inference_classifier.py

--- inference_classifier.py
+++ inference_classifier.py
@@ -14,7 +14,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'L'}
+labels_dict = {0: 'Bullying', 1: 'Harassment', 2: 'Danger'}
 while True:
 
     data_aux = []
